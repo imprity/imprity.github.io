127b6c37f855fb43079c925063547cb34c386558
imprity <51771679+imprity@users.noreply.github.com>
2020-10-13 07:13:28 +0900

    t

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 window.onload = function(){
     let tempConsole = document.getElementById('tempConsole');
-    navigator.bluetooth.requestDevice({
+    /*navigator.bluetooth.requestDevice({
         filters: [{
           services: [0x1234, 0x12345678, '99999999-0000-1000-8000-00805f9b34fb']
         }]
@@ -8,7 +8,8 @@ window.onload = function(){
       .then(device => {
           tempConsole.innerHTML = device;
       })
-      .catch(error => { tempConsole.innerHTML = error.toString() });
+      .catch(error => { tempConsole.innerHTML = error.toString() });*/
+      tempConsole.innerHTML = "some shit";
 }
 
 
