684737a28cbd9da72726ac639d1a7c78f44839bc
imprity <51771679+imprity@users.noreply.github.com>
2020-10-13 07:10:15 +0900

    test

--- main.js
+++ main.js
@@ -5,8 +5,10 @@ window.onload = function(){
           services: [0x1234, 0x12345678, '99999999-0000-1000-8000-00805f9b34fb']
         }]
       })
-      .then(device => { /* ... */ })
-      .catch(error => { console.log(error); });
+      .then(device => {
+          tempConsole.innerHTML = device;
+      })
+      .catch(error => { tempConsole.innerHTML = error });
 }
 
 
